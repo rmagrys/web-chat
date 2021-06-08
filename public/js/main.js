7de4f302fe76937b08ff7f5d420d1cda8db500d4
Rafal Magrys <r.magrys@wp.pl>
2021-06-09 00:50:20 +0200

    added websocket rooms and user functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 const  chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
 const url = "http://localhost:3100";
 
 const {username, room} = Qs.parse(location.search, {
@@ -11,6 +14,12 @@ console.log(username, room);
 const socket = io(url);
 
 socket.emit('joinRoom', {username, room});
+
+
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    addUsers(users);
+})
 
 socket.on('message', message => {
     console.log(message);
@@ -38,4 +47,14 @@ function outputMessage(message) {
                              ${message.text}
                         </p>`;
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+function addUsers(users) {
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
 }
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ io.on('connection', (socket) => {
     socket.on('joinRoom', ( {username, room}) => {
         const user = userJoin(socket.id ,username ,room);
 
+        socket.join(user.room);
+
         socket.emit(
             'message',
             formatMessage(botName, 'Welcome to WebChat')
@@ -25,22 +27,33 @@ io.on('connection', (socket) => {
             .to(user.room)
             .emit('message', formatMessage(botName,`A ${user.username} has joined the chat`)
         );
+
+        io.to(user.room).emit('roomUsers' , {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
     })
 
     socket.on('chatMessage', message => {
         const user = getCurrentUser(socket.id);
         io
             .to(user.room)
-            .emit('message', formatMessage(user.username,message));
+            .emit('message', formatMessage(user.username, message));
     })
 
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
-
         if(user){
+            socket.leave(user.room)
             io
                 .to(user.room)
-                .emit('message', formatMessage(botName,`A ${user.username} has left the chat`));
+                .emit('message', formatMessage(botName,`A ${user.username} has left the chat`)
+                );
+
+            io.to(user.room).emit('roomUsers' , {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
         }
 
     })
